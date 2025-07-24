2a4de02ca3ec591bde37a64e87023bbfae4a30e0
Vedant Malaikar <vmalaikar@gmail.com>
2025-07-24 16:24:43 +0530

    Only one component needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { InfiniteScrollComponent } from './infinite-scroll/infinite-scroll.component';
 
 export const routes: Routes = [
-    { path: 'home', component: AppComponent },
-    { path: 'scroll', component: InfiniteScrollComponent },
+    { path: '', component: InfiniteScrollComponent }
 ];
