54fc34b1e168abfc2d71ffcb516fd2d7edcbc8b2
Vedant Malaikar <vmalaikar@gmail.com>
2025-07-24 18:24:55 +0530

    Added Cors and simulating loading

--- Api/Items.Api/Program.cs
+++ Api/Items.Api/Program.cs
@@ -3,8 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
 var items = Enumerable.Range(1, 1000)
                .Select(index => $"Items {index}")
                .ToArray();
@@ -17,8 +19,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()); 
+
 app.MapGet("/items", (int page, int pageSize) =>
 {
+    Thread.Sleep(1000); // simulate loading
     var response = items.Skip((page - 1) * pageSize).Take(pageSize).ToArray();
 
     return response;
